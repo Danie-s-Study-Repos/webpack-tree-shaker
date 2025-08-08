0e2c87445ed3a8e92dd9a2cd6a062cfc03dafe0f
Danielle Morvant <daniellemorvant1@gmail.com>
2025-08-07 19:21:45 -0500

    webpack.config.js functions add

---
+++ webpack-config.js
@@ -0,0 +1,14 @@
+export functionA(a,b) {
+    return a * b;
+}
+
+export functionB() {
+    console.log("Hello World!");
+}
+
+export functionC(greetings) {
+    const people = [Rosetta, Tabaskas, Clemintana]
+    return.array.forEach(people => {
+    console.log("Hello" + people)   
+    });
+}
